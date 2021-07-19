3e8119ff5cb90575fb814cedae573880f0c3e2b7
Nina NJul <nina.dev.web@gmail.com>
2021-07-19 20:40:23 +0300

    feat: add new information

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-window.onload = function() {
-  /* Click the button to get the square root of each element in the array. */
+window.onload = function () {
+  /* Click the button to get the square root of each element in the array */
   const numbers = [4, 9, 16, 25];
-  console.log(numbers);
+  const squareRoot = document.getElementById('square-root');
+  const squareRootResult = document.getElementById('square-root-result');
 
-  document.getElementById("button1").onclick = function() {
-    document.getElementById("squareRoot").innerHTML = numbers
-      .map(Math.sqrt)
-      .join(", ");
+  squareRoot.onclick = function () {
+    squareRootResult.innerHTML = numbers.map(Math.sqrt).join(', ');
+    squareRoot.disabled = true;
+    squareRoot.classList.add('light-title');
   };
 };
